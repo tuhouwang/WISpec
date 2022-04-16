070bb4024d8b3dbad9c8d764b5a45168ffebea55
Tu Houwang <tuhouwang96@163.com>
2022-04-16 21:44:58 +0800

    稍作修改

--- Pcolor.m
+++ Pcolor.m
@@ -7,6 +7,6 @@ function Pcolor(r, z, tl, casename, tlmin, tlmax)
     xlabel('Range (m)'); ylabel('Depth (m)');
     colorbar('YDir','Reverse','FontSize', 16);
     set(gca,'Position',[0.1,0.15,0.75,0.75],'FontSize',16);
-    set(gcf,'Position',[100,100,800,800]); 
+    set(gcf,'Position',[100,100,800,600]);   
 
 end
--- WISpec.m
+++ WISpec.m
@@ -4,24 +4,26 @@ clear
 tic;
 
 [casename, Layers, Ns, kmax, M, freq, zs, dz, rmax, dr, tlmin, tlmax,...
- dep, c, rho, alpha, Lb, ch, rhoh, alphah] = ReadEnvParameter('input_munk.txt');
+ dep, c, rho, alpha, Lb, ch, rhoh, alphah] = ReadEnvParameter('input_pekeris.txt');
 
-%在声源深度上增加一个虚拟的界面
+%Add a virtual interface to the depth of the sound source
 [dep, c, rho, alpha, Layers, Ns, R] = VirtualInterface(dep, c, rho, alpha, zs, Layers, Ns);
 
 [c, rho, alpha] = ChebInterpolation(dep, c, rho, alpha, Layers, Ns);
 
 [r, k, kh] = ChebInitialization(Layers, freq, rmax, dr, c, alpha, ch, alphah);
 
+%Set up complex contour
 k0 = max(real(k{1}));
 kr = linspace(0, kmax * k0, M);
 eps= 1.5 * kmax * k0 / pi / (M - 1) / log10(exp(1.0));
 kr = kr - 1i * eps;
+
 %------------------------------Depth equation------------------------------
 z   = 0 : dz : dep{end}(end);
 psi = zeros(length(z), M);
 for m = 1 : M
-
+    m
     Vec = ChebDepthSolution(Lb, Ns, Layers, dep, k, rho, kh, rhoh, kr(m), R);
 
     psi(:, m) = KernelFunc(Vec, dz, dep, Layers);
@@ -31,6 +33,7 @@ end
 % Plot(kr, psi(73,:));
 toc;
 %--------------------------Wavenumber Integration--------------------------
+%Ponit source
 phi = zeros(length(z),length(r));
 for ir = 1 : length(r)
     for iz = 1 : length(z)
@@ -39,12 +42,13 @@ for ir = 1 : length(r)
     end
 end
 
-%由位移势函数求声压
+%Line Source
 
+%Sound pressure from displacement potential function
 phi0 = exp(1i * k0) / 4 / pi;
-tl   = - 20 * log10(abs(phi / phi0 ));
-
-Pcolor(r,z,tl,casename,tlmin,tlmax);
+tl   = - 20 * log10(abs(phi / phi0));
 
 % Plot(r, tl(73,:));
+Pcolor(r,z,tl,casename,tlmin,tlmax);
+
 toc;
