5312c65cd97cfd3e710c4c24826c8e0d24c825ef
Tu Houwang <tuhouwang96@163.com>
2022-04-29 11:07:37 +0800

    把波数积分写成了矩阵形式，大大加快了计算速度！

--- WISpec.m
+++ WISpec.m
@@ -4,7 +4,7 @@ clear
 tic;
 
 [casename, Src, Layers, Ns, kmax, M, freq, zs, dz, rmax, dr, tlmin, tlmax,...
- dep, c, rho, alpha, Lb, ch, rhoh, alphah] = ReadEnvParameter('input_bucker.txt');
+ dep, c, rho, alpha, Lb, ch, rhoh, alphah] = ReadEnvParameter('input_munk.txt');
 
 %Get the z and rho of the final resolution
 [z, rhoi] = FinalResolute(dep, dz, rho, Layers);
@@ -24,7 +24,7 @@ kr  = kr - 1i * eps;
 
 %------------------------------Depth equation------------------------------
 psi = zeros(length(z), M);
-for m = 1 : M
+parfor m = 1 : M
     m
     Vec = ChebDepthSolution(Lb, Ns, Layers, dep, k, rho, kh, rhoh, kr(m), R);
 
@@ -32,37 +32,24 @@ for m = 1 : M
 
 end
 
-Plot(kr, psi(201,:));
+% Plot(kr, psi(201,:));
 toc;
 %--------------------------Wavenumber Integration--------------------------
-phi = zeros(length(z),length(r));
 if(Src == 'P')
     % Ponit source
-    for ir = 1 : length(r)
-        r(ir)
-        for iz = 1 : length(z)
-            kernel = psi(iz, :) .* besselj(0, kr * r(ir)) .* real(kr);
-            phi(iz, ir) = trapz(kr, kernel);
-        end
-    end
+    phi  = kmax * k0 / (M - 1) * psi * diag(kr) * besselj(0, kr.' * r); 
     phi0 = exp(1i * k{s}(end)) / 4 / pi;
 else    
-    %Line Source
-    for ir = 1 : length(r)
-        r(ir)
-        for iz = 1 : length(z)
-            kernel = psi(iz, :) .* exp(1i * kr * r(ir));
-            phi(iz, ir) = trapz(kr, kernel);
-        end
-    end
-%     phi0 = 1i / 4 * besselh(0, 1, k{s}(end));
+    % Line Source
+    phi  = kmax * k0 / (M - 1) * psi * exp(1i * kr.' * r);
     phi0 = 1i / 4 * besselh(0, 1, 1);
+%     phi0 = 1i / 4 * besselh(0, 1, k{s}(end));
 end
 
 %Sound pressure from displacement potential function
-phi  = w ^ 2 * diag(rhoi) * phi;
-phi0 = w ^ 2 * rho{s}(end)* phi0;
-tl   = - 20 * log10(abs(phi / phi0));
+phi  = w ^ 2 * diag(rhoi)  * phi;
+phi0 = w ^ 2 * rho{s}(end) * phi0;
+tl   = - 20  * log10(abs(phi / phi0));
 
 % Plot(r, tl(201,:));
 Pcolor(r, z, tl, casename, tlmin, tlmax);
