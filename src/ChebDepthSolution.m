5cd1fced5b68b8e2339b496cf9b0a12b8b3b07c2
Tu Houwang <tuhouwang96@163.com>
2023-05-30 10:06:21 +0800

    release version!

--- src/ChebDepthSolution.m
+++ src/ChebDepthSolution.m
@@ -20,39 +20,39 @@ function Vec = ChebDepthSolution(Lb, Nl, Layers, dep, k, rho, kh, rhoh, kr, R)
         n = n + Nl(i) - 1;
     end  
     
-    % boundary condition
+    % boundary condition.
     n = 1;
     for i = 1 : Layers - 1
-        %displacement potential function 
+        % displacement potential function.
         Pu = -2 / (dep{i}(end) - dep{i}(1)) * ((-1.0).^(0 : Nl(i)))  * Dsave{i};
         Pd =  2 / (dep{i+1}(end) - dep{i+1}(1)) * ones(1, Nl(i+1)+1) * Dsave{i+1};
         
-        %sound pressure is continuous
+        % sound pressure is continuous.
         U(sum(Nl-1)+2*i-1,n:n+Nl(i)-2)   =  rho{i}(end) * (-1.0).^(0:Nl(i)-2);
         U(sum(Nl-1)+2*i-1,sum(Nl-1)+2*i-1:sum(Nl-1)+2*i) = rho{i}(end) * (-1.0).^(Nl(i)-1:Nl(i));
-        %second interface boundary
+        % second interface boundary.
         U(sum(Nl-1)+2*i, n:n+Nl(i)-2) = Pu(1:Nl(i)-1);
         U(sum(Nl-1)+2*i, sum(Nl-1)+2*i-1:sum(Nl-1)+2*i) = Pu(Nl(i):Nl(i)+1);
         
         n = n + Nl(i) - 1;
-        %sound pressure is continuous
+        % sound pressure is continuous.
         U(sum(Nl-1)+2*i-1,n:n+Nl(i+1)-2  )   =  -rho{i+1}(1);
         U(sum(Nl-1)+2*i-1,sum(Nl-1)+2*i+1:sum(Nl-1)+2*i+2)   =  -rho{i+1}(1);
-        %second interface boundary
+        % second interface boundary.
         U(sum(Nl-1)+2*i,  n:n+Nl(i+1)-2) = Pd(1:Nl(i+1)-1);
         U(sum(Nl-1)+2*i,  sum(Nl-1)+2*i+1:sum(Nl-1)+2*i+2) = Pd(Nl(i+1):Nl(i+1)+1);
 
     end  
 
-    %upper boundary, pressure-free boundary
+    % upper boundary, pressure-free boundary.
     U(end-1, 1          :    Nl(1)-1) = 1.0;
     U(end-1, sum(Nl-1)+1:sum(Nl-1)+2) = 1.0;
     
-    %lower boundary
-    %perfectly free / rigid
+    % lower boundary.
+    % perfectly free / rigid.
     Low = (-1.0) .^ (0 : Nl(end));
     if(Lb == 'R')        
-        Low = Low * D;%这里的D是最后一个D，恰好没被覆盖
+        Low = Low * D; % Last D，coincidentally not covered.
     elseif(Lb == 'A')
         Low = rho{end}(end) * sqrt(kr ^ 2 - kh ^ 2) * Low - ... 
               Low * D * 2.0 / (dep{end}(end) - dep{end}(1)) * rhoh;
--- src/WISpec.m
+++ src/WISpec.m
@@ -1,31 +1,60 @@
+% Ocean acoustics.
+
+% Copyright (C) 2023 Houwang Tu
+% ------------------------------------------------------------------------|
+% This program is free software: you can redistribute it and/or modify it |
+% under the terms of the GNU General Public License as published by the   |
+% Free Software Foundation, either version 3 of the License, or (at your  |
+% option) any later version.                                              |
+%                                                                         |
+% This code is distributed in the hope that it will be useful, but without|
+% any warranty; without even the implied warranty of merchantability or   |
+% fitness for a particular purpose. See the GNU General Public License for|
+% more details.                                                           |
+%                                                                         |
+% You should have received a copy of the GNU General Public License along |
+% with this program. If not, see <http://www.gnu.org/licenses/>.          |
+%                                                                         |
+% The program WISpec.m computes the acoustic field in arbitrary           |
+% horizontally stratified media using the Chebyshev--Tau spectral method. |
+% The method is described in the article (H. Tu, Y. Wang, W. Liu et al.,  |
+% A spectral method for the depth-separated solution of a wavenumber      |
+% integration model for horizontally stratified fluid acoustic waveguides |
+% Physics of Fluids, 2023(35), 057127, https://doi.org/10.1063/5.0150221).|
+% under the supervision of Prof. Yongxian Wang, National University of    |
+% Defense Technology, China.                                              |                                                  
+% ------------------------------------------------------------------------|
+
 clc
 clear
-% close all
+close all
 tic;
 
 [casename, Src, Layers, Nl, kmax, M, freq, zs, dz, rmax, dr, tlmin, tlmax,...
  dep, c, rho, alpha, Lb, ch, rhoh, alphah] = ReadEnvParameter('input.txt');
 
-%Get the z and rho of the final resolution
+% Get the z and rho of the final resolution.
 [z, rhoi] = FinalResolute(dep, dz, rho, Layers);
 
-%Add a virtual interface to the depth of the sound source
-[dep, c, rho, alpha, Layers, Nl, R, s] = VirtualInterface(dep, c, rho, alpha, zs, Layers, Nl);  
+% Add a virtual interface to the depth of the sound source.
+[dep, c, rho, alpha, Layers, Nl, R, s] = VirtualInterface(dep, c, rho, ...
+ alpha, zs, Layers, Nl);
 
 [c, rho, alpha] = ChebInterpolation(dep, c, rho, alpha, Layers, Nl);
 
 [r, k, kh, w] = ChebInitialization(Layers, freq, rmax, dr, c, alpha, ch, alphah);
 
-%Set up complex contour
+% Set up complex contour.
 k0  = max(real(k{1}));
 kr  = linspace(0, kmax * k0, M);
 eps = 1.5 * kmax * k0 / pi / (M - 1) / log10(exp(1.0));
 kr  = kr - 1i * eps;
+
 %------------------------------Depth equation------------------------------
 Vec  = cell(Layers, M);
 Vec2 = cell(Layers, 1);
 for m = 1 : M
-%     m
+    m
     Vec(:, m) = ChebDepthSolution(Lb, Nl, Layers, dep, k, rho, kh, rhoh, kr(m), R);
 end
 
@@ -37,6 +66,7 @@ psi = KernelFunc(Vec2, dz, dep, Layers);
 
 % Plot(kr, psi(201,:));
 % Pcolor(real(kr), z, abs(psi), casename, 0, 5);colormap(jet);
+
 toc;
 %--------------------------Wavenumber Integration--------------------------
 if(Src == 'P')
@@ -50,7 +80,7 @@ else
 %     phi0 = 0.25 * 1i * besselh(0, 1, k{s}(end));
 end
 
-%Sound pressure from displacement potential function
+% Sound pressure from displacement potential function.
 phi  = w ^ 2 * diag(rhoi)  * phi;
 phi0 = w ^ 2 * rho{s}(end) * phi0;
 tl   = - 20  * log10(abs(phi / phi0));
